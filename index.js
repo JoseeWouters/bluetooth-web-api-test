f81a65d4b3754366d10c65d0f77f7e415e4f3f14
Josee <info@joseewouters.nl>
2018-12-26 20:04:26 +0100

    Add an event listener to button A.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const connectButton = document.getElementById('js-connect');
+const hotOrCold = document.getElementById('js-hot');
+const waitForMicrobit = document.getElementById('js-wait-for-it');
 
-connectButton.addEventListener('click', () => {
+hotOrCold.addEventListener('click', () => {
     navigator.bluetooth.requestDevice({
         filters: [
             { namePrefix: 'BBC micro:bit' } 
         ],
-        optionalServices: ['e95d6100-251d-470a-a062-fa1922dfa9a8']
+        optionalServices: ['e95d6100-251d-470a-a062-fa1922dfa9a8'] // temperature service
     })
     .then(device => {
         console.log(device);
@@ -30,3 +31,36 @@ connectButton.addEventListener('click', () => {
         console.error(error);
     })
 });
+
+waitForMicrobit.addEventListener('click', () => {
+    navigator.bluetooth.requestDevice({
+        filters: [
+            { namePrefix: 'BBC micro:bit' } 
+        ],
+        optionalServices: ['e95d9882-251d-470a-a062-fa1922dfa9a8'] // button service
+    })
+    .then(device => {
+        console.log(device);
+        return device.gatt.connect()
+    })
+    .then(server => {
+        console.log(server);
+        return server.getPrimaryService('e95d9882-251d-470a-a062-fa1922dfa9a8');
+    })
+    .then(service => {
+        console.log(service);
+        return service.getCharacteristic('e95dda90-251d-470a-a062-fa1922dfa9a8');
+    })
+    .then(characteristic => characteristic.startNotifications())
+    .then(characteristic => {
+        console.log(characteristic);
+        characteristic.addEventListener('characteristicvaluechanged', waitForButtonA)
+    })
+    .catch(error => {
+        console.error(error);
+    })
+})
+
+function waitForButtonA() {
+    console.log('Button A pressed and released');
+}
--- readme.md
+++ readme.md
@@ -2,9 +2,13 @@
 
 Make sure the default program is loaded on the micro:bit with all services enabled.
 
+The button 'Hot or Cold' logs the temperature of the micro:bit.
+
+The button 'Wait for it' adds an eventlistener to Button A.
+
 # Read more
 
-- [Overview of BLE Characteritisch from the micro:bit](https://lancaster-university.github.io/microbit-docs/ble/profile/)
+- [Overview of BLE Characteristic from the micro:bit](https://lancaster-university.github.io/microbit-docs/ble/profile/)
 - [Google about Web Bluetooth API](https://developers.google.com/web/updates/2015/07/interact-with-ble-devices-on-the-web)
 - [MDN about Web Bluetooth API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API)
 - [Fronteers tutorial over Web Bluetooth API](https://fronteers.nl/blog/2018/12/beginnen-met-webbluetooth)
